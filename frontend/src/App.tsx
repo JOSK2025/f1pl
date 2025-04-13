263644a5b8e17fef3e0341aa61ad6653de9874fe
jacek <jacek.konieczny9@gmail.com>
2025-04-13 20:33:22 +0200

    add routing and tailwind setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
+
 function App() {
     return (
-        <div>
-            app
+        <div className="min-h-screen bg-stone-50">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </Router>
         </div>
   )
 }
