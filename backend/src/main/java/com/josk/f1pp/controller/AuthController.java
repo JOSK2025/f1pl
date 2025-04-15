3059d80eda6400da1f4697a28860751dba703e61
Olaf <szyncelolaf@gmail.com>
2025-04-15 20:19:21 +0200

    dodano globalnie cors

--- backend/src/main/java/com/josk/f1pp/controller/AuthController.java
+++ backend/src/main/java/com/josk/f1pp/controller/AuthController.java
@@ -18,13 +18,11 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/api/auth/register")
     public ResponseEntity<?> register(@RequestBody RegisterDto request) {
         if(request.username == null || request.username.isEmpty()) {
