e993d1500808699345b9dfa63fcd1f38321e9b4e
Olaf <szyncelolaf@gmail.com>
2025-04-16 19:02:24 +0200

    Poprawiono działanie cors

---
+++ backend/src/main/java/com/josk/f1pp/WebConfig.java
@@ -0,0 +1,18 @@
+package com.josk.f1pp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // wszystkie ścieżki
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // jeśli chcesz wysyłać cookies
+    }
+}
--- backend/src/main/java/com/josk/f1pp/dto/RegisterDto.java
+++ backend/src/main/java/com/josk/f1pp/dto/RegisterDto.java
@@ -1,10 +1,11 @@
 package com.josk.f1pp.dto;
-
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 @Data
 public class RegisterDto {
     public String username;
+    @Email
     public String email;
     public String password;
 }
